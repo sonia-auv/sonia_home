11c7dc3ce653df35f5b46120ba6815efa3e6aea3
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2025-09-20 15:43:15 -0400

    should be a good starting point

--- sonia_setup.bash
+++ sonia_setup.bash
@@ -4,11 +4,28 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ENV_FILE="$SCRIPT_DIR/.env"
 
+
+
 # Check if the .env file exists
 if [[ -f "$ENV_FILE" ]]; then
     echo ".env file already exists at $ENV_FILE"
 else
     echo ".env file not found. Creating it..."
     echo "SONIA_WS=$HOME/ros2_sonia_ws" > "$ENV_FILE"
+    
+    UBUNTU_VERSION=$(lsb_release -rs)
+
+    if [[ "$UBUNTU_VERSION" == "20.04" ]]; then
+        # echo "Detected Ubuntu 20.04"
+        # Do 20.04-specific stuff here
+        export ROS_WS_SETUP=~/ros2_humble/install/setup.bash
+        echo "SONIA_WS_SETUP=$HOME/ros2_humble/install/setup.bash" >> "$ENV_FILE"
+
+    elif [[ "$UBUNTU_VERSION" == "22.04" ]]; then
+        # echo "Detected Ubuntu 22.04"
+        echo "SONIA_WS_SETUP=/opt/ros/humble/setup.bash" >> "$ENV_FILE"
+    else
+        echo "Warning: Unsupported Ubuntu version ($UBUNTU_VERSION)"
+    fi
     echo "Created .env with SONIA_WS=$HOME/ros2_sonia_ws"
 fi
