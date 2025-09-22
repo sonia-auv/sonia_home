8f64ac7afb13ca50b4753bdd6814154e7d3c3ced
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2025-09-22 13:26:23 -0400

    sync all existing projects

--- sonia_setup.bash
+++ sonia_setup.bash
@@ -4,14 +4,13 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ENV_FILE="$SCRIPT_DIR/.env"
 
-
-
 # Check if the .env file exists
 if [[ -f "$ENV_FILE" ]]; then
     echo ".env file already exists at $ENV_FILE"
 else
     echo ".env file not found. Creating it..."
     echo "SONIA_WS=$HOME/ros2_sonia_ws" > "$ENV_FILE"
+    echo "Added SONIA_WS=$HOME/ros2_sonia_ws to .env file"
     
     UBUNTU_VERSION=$(lsb_release -rs)
 
@@ -19,11 +18,13 @@ else
         # echo "Detected Ubuntu 20.04"
         # Do 20.04-specific stuff here
         export ROS_WS_SETUP=~/ros2_humble/install/setup.bash
-        echo "SONIA_WS_SETUP=$HOME/ros2_humble/install/setup.bash" >> "$ENV_FILE"
+        echo "ROS_WS_SETUP=$HOME/ros2_humble/install/setup.bash" >> "$ENV_FILE"
+        echo "Added ROS_WS_SETUP=$HOME/ros2_humble/install/setup.bash to .env file"
 
     elif [[ "$UBUNTU_VERSION" == "22.04" ]]; then
         # echo "Detected Ubuntu 22.04"
-        echo "SONIA_WS_SETUP=/opt/ros/humble/setup.bash" >> "$ENV_FILE"
+        echo "ROS_WS_SETUP=/opt/ros/humble/setup.bash" >> "$ENV_FILE"
+        echo "Added ROS_WS_SETUP=/opt/ros/humble/setup.bash to .env file"
     else
         echo "Warning: Unsupported Ubuntu version ($UBUNTU_VERSION)"
     fi
